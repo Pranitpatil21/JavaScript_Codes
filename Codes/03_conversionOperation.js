150f26e5202e4c12a2f0f74820e7692e98ceefa8
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-10-09 10:49:37 +0530

    Add files via upload

---
+++ Codes/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+//Coverting to number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN -> Not an Number
+//  true => 1; false => 0
+
+
+//Converting to boolean
+
+let isLoggedIn = "hitesh"
+let bolleanIsloggedIn = Boolean(isLoggedIn)
+console.log(typeof bolleanIsloggedIn)
+console.log(bolleanIsloggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+//COnverting to String
+let someNumber = 13213
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
