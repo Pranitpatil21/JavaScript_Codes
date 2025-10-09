73e4cac437bec987a6306fadfa722074da0a4d7b
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-10-09 10:18:11 +0530

    Add files via upload

---
+++ Codes/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert(2+2) //we using node.js , not browser on browser it will work but by using node.js it will not
+
+console.log(3
+    +
+    3)  //Code redabilty should be high not like this should be in proper format 
+
+console.log("Pranit")
+
+let name = "pranit"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint => for to much large values
+// String = " "
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); //object
+
+
