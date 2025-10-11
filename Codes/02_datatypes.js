14f4ca98785bc54a6a83e2c180ba19a24ecd51a7
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-10-11 13:55:15 +0530

    datatypes

--- Codes/02_datatypes.js
+++ Codes/02_datatypes.js
@@ -20,11 +20,12 @@ let state;
 //null => standalone value
 // undefined =>
 // symbol => unique 
-
+  
 
 //object
 
 console.log(typeof undefined) // undefined
 console.log(typeof null); //object
+
 
 
