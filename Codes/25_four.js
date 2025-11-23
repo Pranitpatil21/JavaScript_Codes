342788e181c8b8d893bd45f1c2c9dbede1051c25
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2025-11-24 02:04:38 +0530

    codes

---
+++ Codes/25_four.js
@@ -0,0 +1,37 @@
+/*
+ A for...in loop is used to iterate over the keys (property names) of an object.
+ It works well with:
+  - Objects
+  - Arrays (not recommended)
+  - Custom object properties
+ */
+
+
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for(const key in myObject){
+    // console.log(`${key} shortcut for ${myObject[key]}`);   
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+ const map = new Map()
+    map.set('IN', "India")
+    map.set('USA', "United States of America")
+    map.set('Fr', "France")
+    map.set('IN', "India")
+
+  for (const key in map) {
+      console.log(key);
+  }
+
